f0fc3f42b53cd79f8b5c142743a1698332dce849
Elchin <el.kuliyev@gmail.com>
2022-11-05 15:38:04 +0400

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 OS = os.popen("lsb_release -ds").read()
-print(OS)
+#print(OS)
 #print(type(OS))
 
-if OS in ["Ubuntu 22.04.1 LTS"]:
-	print(Ubuntu)
+if OS in "Ubuntu 22.04.1 LTS":
+	print("Ubuntu")
 else:
 	print("Error")
