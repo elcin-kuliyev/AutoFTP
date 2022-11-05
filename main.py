d154b34e85605f7a5ce5aa7a8b59b77df1770a09
Elchin <el.kuliyev@gmail.com>
2022-11-05 16:56:02 +0400

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ for result.group(0) in ["Ubuntu", "Debian", "Linux Mint", "Kali Linux", "Parrot 
 	# install update and upgrade system
 	check_call("sudo apt-get update ", shell=True)
 	check_call("sudo apt-get upgrade -y ", shell=True)
-
+for result.group(0) in ["CentOS", "Rocky Linux", "AlmaLinux", "Fedora Linux"]:
+	# install update and upgrade system
+	check_call("sudo yum update -y", shell=True)
 
 
