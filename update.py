1977f0ad747d4a7e5603f6da7d23bbc3384e0c28
Elchin <el.kuliyev@gmail.com>
2022-11-05 18:04:45 +0400

    main.py

--- update.py
+++ update.py
@@ -10,8 +10,8 @@ def update_ubuntu():
     check_call("sudo apt-get update ", shell=True)  # update server
     check_call("sudo apt-get upgrade -y ", shell=True)  # upgrade server
 
-def install_vsftpd():
+def ubuntu_vsftpd():
     check_call("sudo apt-get install vsftpd -y", shell=True)  # install FTP server
     check_call("sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.original", shell=True)  # Backup server conf
 
-
+def ubuntu_firewall():
