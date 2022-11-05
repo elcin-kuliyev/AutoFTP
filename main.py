743a7404eed3886b8cc832bb1f1f7058583c0ec4
Elchin <el.kuliyev@gmail.com>
2022-11-05 16:01:13 +0400

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+import subprocess
 OS = os.popen("lsb_release -ds").read()
 #print(OS)
 #print(type(OS))
 
 if OS.strip() in "Ubuntu 22.04.1 LTS\\n":
 	print("Ubuntu")
-	os.popen("sudo apt-get update").read()
+	subprocess.check_call("sudo apt-get update ", shell=True)
+
 else:
 	print("Error")
