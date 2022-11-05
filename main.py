45ec48d21aad2173d2145977366380481a7b34d9
Elchin <el.kuliyev@gmail.com>
2022-11-05 15:53:17 +0400

    main.py

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ OS = os.popen("lsb_release -ds").read()
 
 if OS.strip() in "Ubuntu 22.04.1 LTS\\n":
 	print("Ubuntu")
+	os.popen("sudo apt-get update && sudo apt-get upgrade -y").read()
 else:
 	print("Error")
