6d8e40ff4ddf5b7e53d331b500fc50fb1e152530
Elchin <el.kuliyev@gmail.com>
2022-11-05 17:52:58 +0400

    main.py

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ OS = os.popen("lsb_release -ds").read()
 result = match(r'Ubuntu', OS.strip())
 if result.group(0) == "Ubuntu":
 	update.update_ubuntu()
+	update.install_vsftpd()
--- update.py
+++ update.py
@@ -11,7 +11,7 @@ def update_ubuntu():
     check_call("sudo apt-get upgrade -y ", shell=True)  # upgrade server
 
 def install_vsftpd():
-    check_call("sudo apt-get install vsftpd", shell=True)  # install FTP server
+    check_call("sudo apt-get install vsftpd -y", shell=True)  # install FTP server
     check_call("sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.original", shell=True)  # Backup server conf
 
 
