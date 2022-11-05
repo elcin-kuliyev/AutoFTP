51b9d0b0fcadc62379bcde2a84e01861165068d1
Elchin <el.kuliyev@gmail.com>
2022-11-05 18:15:54 +0400

    main.py

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 from os import popen
 from subprocess import check_call
 from re import match
+import pyufw
 
 OS = popen("lsb_release -ds").read()
 
@@ -15,3 +16,10 @@ def ubuntu_vsftpd():
     check_call("sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.original", shell=True)  # Backup server conf
 
 def ubuntu_firewall():
+    ufw.enable()
+    ufw.add("allow OpenSSH")
+    ufw.add("allow 20/tcp")
+    ufw.add("allow 21/tcp")
+    ufw.add("allow 990/tcp")
+    ufw.add("allow 40000:50000/tcp")
+    check_call("sudo ufw status", shell=True)
